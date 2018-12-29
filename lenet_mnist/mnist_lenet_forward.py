1a12a13a8bf1ad7f021cbeffcbd7eb67af1761f3
maniagea <maniagea@163.com>
2018-12-29 17:16:13 +0800

    add lenet5

---
+++ lenet_mnist/mnist_lenet_forward.py
@@ -0,0 +1,38 @@
+#coding:utf-8
+# 描述网络结构
+import tensorflow as tf
+
+IMAGE_SIZE = 28
+NUM_CHANNELS = 1
+CONV1_SIZE = 5
+CONV1_KERNEL_NUM =32
+CONV2_SIZE = 5
+CONV2_KERNEL_NUM =64
+FC_SIZE = 512
+OUTPUT_NODE = 10
+
+def get_weight(shape, regularizer):
+    w = tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+    if regularizer != None:
+        tf.add_to_collection('losses',tf.contrib.layers.l2_regularizer(regularizer)(w))
+    return w
+
+def get_bias(shape):
+    b = tf.Variable(tf.zeros(shape))
+    return b
+
+def conv2d(x, w):
+    return tf.nn.conv2d(x, w, strides=[1,1,1,1], padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+def forward(x, regularizer):
+    w1 = get_weight([INPUT_NODE, LAYER1_NODE], regularizer)
+    b1 = get_bias([LAYER1_NODE])
+    y1 = tf.nn.relu(tf.matmul(x, w1) + b1)
+
+    w2 = get_weight([LAYER1_NODE, OUTPUT_NODE], regularizer)
+    b2 = get_bias([OUTPUT_NODE])
+    y = tf.matmul(y1, w2) + b2
+    return y
