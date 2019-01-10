74d94018ed80326cb3323578fc607cac8d38d375
maniagea <maniagea@163.com>
2019-01-10 15:34:44 +0800

    add forward

--- lenet_mnist/mnist_lenet_forward.py
+++ lenet_mnist/mnist_lenet_forward.py
@@ -28,43 +28,43 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 def forward(x, regularizer):
-    //初始化第一层卷积核
+    # 初始化第一层卷积核
     conv1_w = get_weight([CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_KERNEL_NUM], regularizer)
-    //初始化第一层偏置
+    # 初始化第一层偏置
     conv1_b = get_bias([CONV1_KERNEL_NUM])
-    //执行卷积计算
+    # 执行卷积计算
     conv1 = conv2d(x, conv1_w)
-    //对卷积后的输出conv1添加偏置conv1_b, 通过relu激活函数
+    # 对卷积后的输出conv1添加偏置conv1_b, 通过relu激活函数
     relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_b))
-    //激活后的输出进行最大池化
+    # 激活后的输出进行最大池化
     pool1 = max_pool_2X2(relu1)
 
-    //初始化第二层卷积核
+    # 初始化第二层卷积核
     conv2_w = get_weight([CONV2_SIZE, CONV2_SIZE, CONV1_KERNEL_NUM, CONV2_KERNEL_NUM], regularizer)
-    //初始化第二层偏置
+    # 初始化第二层偏置
     conv2_b = get_bias([CONV2_KERNEL_NUM])
-    //执行卷积计算，输入是上层的pool1
+    # 执行卷积计算，输入是上层的pool1
     conv2 = conv2d(pool1, conv2_w)
-    //对卷积后的输出conv1添加偏置conv1_b, 通过relu激活函数
+    # 对卷积后的输出conv1添加偏置conv1_b, 通过relu激活函数
     relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_b))
-    //激活后的输出进行最大池化. pool2是第二层的输出，需要把它从三维张量变为二维张量
+    # 激活后的输出进行最大池化. pool2是第二层的输出，需要把它从三维张量变为二维张量
     pool2 = max_pool_2X2(relu2)
 
-    //得到pool2输出矩阵的维度，存入list中
+    # 得到pool2输出矩阵的维度，存入list中
     pool_shape = pool2.get_shape().as_list()
-    //提取特征的长度，宽度，深度，相乘得到所有特征点的个数
+    # 提取特征的长度，宽度，深度，相乘得到所有特征点的个数
     nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]
-    //将pool2表示成batch行，所有特征点作为列的二维形状。把它再喂到全连接网络里
+    # 将pool2表示成batch行，所有特征点作为列的二维形状。把它再喂到全连接网络里
     reshaped = tf.reshape(pool2, [pool_shape[0], nodes])
 
     fc1_w = get_weight([nodes, FC_SIZE], regularizer)
     fc1_b = get_bias([FC_SIZE])
-    //上层的输出乘以本层的权重，加上偏置，过激活函数relu
+    # 上层的输出乘以本层的权重，加上偏置，过激活函数relu
     fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_w) + fc1_b)
-    //如果是训练阶段，对该层的输出使用50%的dropout
+    # 如果是训练阶段，对该层的输出使用50%的dropout
     if train: fc1 = tf.nn.dropout(fc1, 0.5)
     
-    //初始化第二层
+    # 初始化第二层
     fc2_w = get_weight([FC_SIZE, OUTPUT_NODE], regularizer)
     fc2_b = get_bias([OUTPUT_NODE])
     
