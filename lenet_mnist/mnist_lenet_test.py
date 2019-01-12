3d5ffc4ab38480e99792b99b5da21491db691254
lmnbeck <maniagea09@gmail.com>
2019-01-12 15:53:08 +0800

    can run

--- lenet_mnist/mnist_lenet_test.py
+++ lenet_mnist/mnist_lenet_test.py
@@ -8,7 +8,7 @@ import mnist_lenet_forward
 import mnist_lenet_backward
 import numpy as np
 
-TEST_INTERNAL_SECS = 5
+TEST_INTERNAL_SECS = 30
 
 def test(mnist):
     with tf.Graph().as_default() as g:
@@ -19,10 +19,10 @@ def test(mnist):
         mnist_lenet_forward.NUM_CHANNELS # 输入的通道数
         ])
 
-        y_ = tf.placeholder(tf.float32, [None, mnist_forward.OUTPUT_NODE])
-        y = mnist_forward.forward(x, False, None)
+        y_ = tf.placeholder(tf.float32, [None, mnist_lenet_forward.OUTPUT_NODE])
+        y = mnist_lenet_forward.forward(x, False, None)
 
-        ema = tf.train.ExponentialMovingAverage(mnist_backward.MOVING_AVERAGE_DECAY)
+        ema = tf.train.ExponentialMovingAverage(mnist_lenet_backward.MOVING_AVERAGE_DECAY)
         ema_restore = ema.variables_to_restore()
         saver = tf.train.Saver(ema_restore)
 
@@ -31,17 +31,17 @@ def test(mnist):
 
         while True:
             with tf.Session() as sess:
-                ckpt = tf.train.get_checkpoint_state(mnist_backward.MODEL_SAVE_PATH)
+                ckpt = tf.train.get_checkpoint_state(mnist_lenet_backward.MODEL_SAVE_PATH)
                 if ckpt and ckpt.model_checkpoint_path:
                     saver.restore(sess, ckpt.model_checkpoint_path)
                     global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-                    reshaped_x = np.reshape(mnist.test.image,(
+                    reshaped_x = np.reshape(mnist.test.images,(
                         mnist.test.num_examples,        # 每轮喂入多少张图片
                         mnist_lenet_forward.IMAGE_SIZE, # 行分辨率
                         mnist_lenet_forward.IMAGE_SIZE, # 列分辨率
                         mnist_lenet_forward.NUM_CHANNELS # 输入的通道数
                         ))
-                    accuracy_score = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+                    accuracy_score = sess.run(accuracy, feed_dict={x: reshaped_x, y_: mnist.test.labels})
                     print("After %s training steps, test accuracy = %g ") % (global_step, accuracy_score)
                 else:
                     print('No checkpoint file found')
